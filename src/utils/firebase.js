1757fac9a2fbd64e93e8ab351916f1a12a80e68c
Leelavathi Gade <leelaroshangade@gmail.com>
2021-11-16 14:07:32 +0100

    modifications in firebase file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,13 +9,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   
-    // apiKey: "AIzaSyCL82cbY8yOSnDIN_IDCrqZ9cSOGQI2SLg",
-    // authDomain: "react-firebase-auth-33d74.firebaseapp.com",
-    // projectId: "react-firebase-auth-33d74",
-    // storageBucket: "react-firebase-auth-33d74.appspot.com",
-    // messagingSenderId: "807190295398",
-    // appId: "1:807190295398:web:ff2f2f64b554c7e66d0ea7"
-
+ 
 
 
 
